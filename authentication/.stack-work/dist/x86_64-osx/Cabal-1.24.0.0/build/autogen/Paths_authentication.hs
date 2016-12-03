7e0c1a9f1c70b74e0bb58d154f210637687a9978
laurarundle <rundlel@tcd.ie>
2016-12-03 15:10:49 +0000

    start of encryption

---
+++ authentication/.stack-work/dist/x86_64-osx/Cabal-1.24.0.0/build/autogen/Paths_authentication.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
+module Paths_authentication (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/laurarundle/Desktop/distro/authentication/.stack-work/install/x86_64-osx/lts-7.11/8.0.1/bin"
+libdir     = "/Users/laurarundle/Desktop/distro/authentication/.stack-work/install/x86_64-osx/lts-7.11/8.0.1/lib/x86_64-osx-ghc-8.0.1/authentication-0.1.0.0-Jy4JAW6J6uq6YY5W8MyJWl"
+datadir    = "/Users/laurarundle/Desktop/distro/authentication/.stack-work/install/x86_64-osx/lts-7.11/8.0.1/share/x86_64-osx-ghc-8.0.1/authentication-0.1.0.0"
+libexecdir = "/Users/laurarundle/Desktop/distro/authentication/.stack-work/install/x86_64-osx/lts-7.11/8.0.1/libexec"
+sysconfdir = "/Users/laurarundle/Desktop/distro/authentication/.stack-work/install/x86_64-osx/lts-7.11/8.0.1/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "authentication_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "authentication_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "authentication_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "authentication_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "authentication_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
---
+++ authentication/src/Lib.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+module Lib
+    ( startApp
+    ) where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Char
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data Token = Token
+	{ key :: Key
+	, metadata :: String
+	} deriving (Show, Read)
+
+data Key = Key
+	{ key1 :: Int	
+	} deriving(Eq, Show, Read)
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "users" :> Get '[JSON] [User]
+
+
+main = do 
+	let s = "sample string"
+	let x = map ord s
+	print (s)
+	print (x)
+
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
